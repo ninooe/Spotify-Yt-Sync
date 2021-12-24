1def2b2c1a5ca90fcd3fee31c57262ae42659de5
ninooe <oesterreich.nino@gmail.com>
2021-12-24 14:13:34 +0100

    fixed typehinting

--- selenium_helper.py
+++ selenium_helper.py
@@ -1,19 +1,20 @@
+import logging
+from typing import Optional, Type
 
-import selenium
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.remote.webelement import WebElement
-import logging
 
-########### todo Proper Exceptionhandeling in case of connection lost connection ############
+########### TODO Proper Exceptionhandeling in case of connection lost connection ############
 
-def wait_for_element(locator: str, query: str, driver: selenium.webdriver, timeout: int = 30) -> WebElement:
+def wait_for_element(locator: str, query: str, driver: Type[WebDriver], timeout: int = 30) -> Optional[WebElement]:
     """Let selenium driver wait for element
 
     Args:
         locator (str): locator present in this set selenium.webdriver.common.by
         query (str): description of element matching locator
-        driver (webdriver): instance of selenium.webdriver
+        driver (webdriver): instance of class inheriting from selenium.webdriver.remote.webdriber.WebDriver
         timeout (int, optional): time to wait before timeout. Defaults to 30.
 
     Returns:
@@ -24,6 +25,6 @@ def wait_for_element(locator: str, query: str, driver: selenium.webdriver, timeo
                 EC.presence_of_element_located((locator, query))
             )
         return parentElement
-    except Exception:
-        logging.debug(f"{Exception} occured while waiting for element")
+    except Exception as err:
+        logging.debug(f"{err} occured while waiting for element")
         return None
